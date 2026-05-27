149d81b3a75a1add752c87aa3afcca9ad8939d46
Jon <coryellj1@students.westerntc.edu>
2026-05-27 15:18:13 -0500

    Part 3 Complete

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,17 +1,13 @@
 import Header from '../components/Header'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+import MainLayout from './MainLayout'
 
 const ContactPage = () => {
   return (
-    <>
-        <Header />
-        <NavBar />
-        <main>
-            <h1>Contact Page</h1>
-        </main>
-        <Footer />
-    </>
+    <MainLayout>
+        <h1>Contact Page</h1>
+    </MainLayout>
     )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,14 @@
 import Header from "../components/Header"
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
+import MainLayout from "./MainLayout"
 
 
 const HomePage = () => {
   return (
-    <>
-        <Header />
-        <NavBar />
-        <main>
-            <h1>Home Page</h1>
-        </main>
-        <Footer /> 
-    </>
+    <MainLayout>
+      <h1>Home Page</h1>
+    </MainLayout>
   )
 }
 
